6063f2090283f35b5e0423b0be4ee3b54a5a36fc
Manuel Ramon <m.ramon.fernandez@gmail.com>
2024-02-02 21:22:34 +0100

    update makesymlinks.sh info

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -23,7 +23,8 @@ files="bash_profile zshrc Rprofile tmux.conf"    # list of files/folders to syml
 # cd $dir
 # echo "done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir 
+# to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
     #echo "Moving any existing dotfiles from ~ to $olddir"
     #mv ~/.$file ~/dotfiles_old/
